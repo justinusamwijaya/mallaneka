b97368a641448b42897668365f2e366bc366839d
stedyyulius <stedyyulius@gmail.com>
2017-07-31 17:08:00 +0700

    carousel,menubar,description done

---
+++ client/src/components/Body.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    <Description></Description>
+    <Templates></Templates>
+  </div>
+</template>
+
+<script>
+import Description from './Description'
+import Templates from './Templates'
+
+export default {
+  components:{
+    Description,Templates
+  }
+}
+</script>
+
+<style lang="css">
+</style>
